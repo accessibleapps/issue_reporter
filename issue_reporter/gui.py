3cca99b5575b63270709b107b8da2fcc88cd3774
Christopher Toth <q.alpha@gmail.com>
2014-03-09 18:13:51 -0600

    Buttons!

--- issue_reporter/gui.py
+++ issue_reporter/gui.py
@@ -10,6 +10,7 @@ class IssueReporterDialog(wx_forms.AutoSizedDialog):
  steps = fields.Text(label=__("Steps to Reproduce"), multiline=True, size=(600, 600))
  expected = fields.Text(label=__("Expected Results"), multiline=True, size=(800, 300))
  actual = fields.Text(label=__("Actual Results"), multiline=True, size=(800, 300))
+ buttons = fields.ButtonSizer(ok=True, cancel=True)
 
  def get_report(self):
   return issue_reporter.Report(email=self.email.get_value(), summary=self.summary.get_value, frequency=self.frequency.get_value(), steps=self.steps.get_value(), expected_results=self.expected_results.get_value(), actual_results=self.actual_results.get_value())
