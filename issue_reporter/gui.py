bfb6c70b00b51eaf9331d1a01f9b1972e9266103
Christopher Toth <q.alpha@gmail.com>
2014-03-09 18:10:33 -0600

    Fixes to gui and etc.

--- issue_reporter/gui.py
+++ issue_reporter/gui.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 from . import issue_reporter
+from wx_utils import forms as wx_forms
+from gui_builder import fields, forms
 
-class ReportWindow(wx_forms.AutoSizedFrame);
+class IssueReportDialog(wx_forms.AutoSizedDialog):
  email = fields.Text(label=__("Your &Email Address"), size=(600, 200))
  summary = fields.Text(label=__("Summary"), size=(800, 200))
- frequency = fields.RadioButtonGroup(label=__("How frequently does the Issue occur?", choices=[__("Rarely"), __("Sometimes"), __("Frequently"), __("Always")])
+ frequency = fields.RadioButtonGroup(label=__("How frequently does the Issue occur?"), choices=[__("Rarely"), __("Sometimes"), __("Frequently"), __("Always")])
  steps = fields.Text(label=__("Steps to Reproduce"), multiline=True, size=(600, 600))
  expected = fields.Text(label=__("Expected Results"), multiline=True, size=(800, 300))
  actual = fields.Text(label=__("Actual Results"), multiline=True, size=(800, 300))
