f874beb2265771e3d8365264823e5d6539659932
Q <q@q-continuum.net>
2014-03-09 18:05:46 -0600

    Fixed setup script and imported IssueReporter and Report at top lvel.

---
+++ issue_reporter/__init__.py
@@ -0,0 +1,1 @@
+from issue_reporter import IssueReporter, Report
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
  name = 'issue_reporter',
  version = 0.1,
- description = """Simple interface for quickly reporting issues with a GUI and Mailgun out the back"""
+ description = """Simple interface for quickly reporting issues with a GUI and Mailgun out the back""",
  packages = find_packages(),
  install_requires = ['sysinfo', 'mailgun_api'],
 )
