a176bced94aec867b0db6a96194603935c66c0ff
Christopher Toth <q.alpha@gmail.com>
2024-06-22 13:12:50 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
  name = 'issue_reporter',
- version = 0.1,
+ version = '0.1.1',
  description = """Simple interface for quickly reporting issues with a GUI and Mailgun out the back""",
  packages = find_packages(),
  package_data={'issue_reporter': [
